125380f65e8976f1fa6b8d176a3ab9d0b53bbc5a
Daniel Bigos <daniel.bigos@icloud.com>
2024-08-21 07:51:21 +0200

    ref: spli set/get

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,14 @@ extern crate alloc;
 #[global_allocator]
 static ALLOC: mini_alloc::MiniAlloc = mini_alloc::MiniAlloc::INIT;
 
+use alloy_sol_types::sol;
+use stylus_sdk::evm;
 use stylus_sdk::prelude::{entrypoint, external, sol_storage};
+
+sol! {
+    #[allow(missing_docs)]
+    event DataSet(string data);
+}
 
 sol_storage! {
     #[entrypoint]
@@ -23,8 +30,12 @@ impl StringPoC {
         String::new()
     }
 
-    pub fn pass(&mut self, data: String) -> String {
-        self._data.set_str(data);
+    pub fn set(&mut self, data: String) -> Result<(), Vec<u8>> {
+        self._data.set_str(data.clone());
+        evm::log(DataSet { data });
+        Ok(())
+    }
+    pub fn pass(&self) -> String {
         self._data.get_string()
     }
 }
